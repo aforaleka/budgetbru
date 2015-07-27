e8126f5b26be6d22fd7ec8367030d4b3a2370972
aforaleka <aforaleka@gmail.com>
2015-07-27 20:18:24 +0200

    hello

--- app/controllers/expenditures_controller.rb
+++ app/controllers/expenditures_controller.rb
@@ -5,20 +5,56 @@ class ExpendituresController < ApplicationController
   end
 
   def new
-    @expenditure = Expenditure.new
+    if user_signed_in?
+      @expenditure = Expenditure.new
+    else
+      redirect_to new_user_session_path
+      flash.notice = "sign in first please"
+    end
   end
 
   def create
-    @expenditure = Expenditure.new(expenditure_params)
+    @expenditure = current_user.expenditures.build expenditure_params
     if @expenditure.save
-      redirect_to expenditure_path(@expenditure)
+      redirect_to @expenditure
     else
-      redirect_to new_expenditure_path
+      render :new
     end
   end
 
   def show
     @expenditure = Expenditure.find(params[:id])
+  end
+
+  def edit
+    @expenditure = Expenditure.find params[:id]
+    if @expenditure.user == current_user
+      render 'edit'
+    else
+      redirect_to expenditures_path
+      flash.alert = "Invalid Permissions"
+    end
+  end
+
+  def update
+    @expenditure = Expenditure.find params[:id]
+    if @expenditure.update expenditure_params
+      redirect_to @expenditure
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @expenditure = Expenditure.find params[:id]
+    if @expenditure.user = current_user
+      @expenditure.destroy
+      redirect_to expenditures_path
+      flash.notice = "successfully deleted"
+    else
+      redirect_to expenditures_path
+      flash.alert = "Invalid Permissions"
+    end
   end
 
   private
